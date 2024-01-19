ed45bb648efb9d9d2c5d5c51afd88566622c0213
userfiona <fionaramaesela.com>
2024-01-19 02:57:44 +0000

    w

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,15 +1,7 @@
--- Initial
-DROP TABLE IF EXISTS items;
-DROP TABLE IF EXISTS orders;
+-- Creates a trigger that decreases the quantity of an item after adding a new order
 
-CREATE TABLE IF NOT EXISTS items (
-    name VARCHAR(255) NOT NULL,
-    quantity int NOT NULL DEFAULT 10
-);
-
-CREATE TABLE IF NOT EXISTS orders (
-    item_name VARCHAR(255) NOT NULL,
-    number int NOT NULL
-);
-
-INSERT INTO items (name) VALUES ("apple"), ("pineapple"), ("pear");
+CREATE TRIGGER decrement
+AFTER INSERT
+ON orders
+FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE NAME = NEW.item_name;
