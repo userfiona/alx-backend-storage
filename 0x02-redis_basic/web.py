24269f12664988999fba547a993bf9d07dae707d
userfiona <fionaramaesela.com>
2024-01-24 15:54:07 +0000

    w

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,57 +1,30 @@
 #!/usr/bin/env python3
-
-"""
-This module provides the class Cache
-"""
-
+"""Implement an expiring web cache and tracker"""
+from datetime import timedelta
+from typing import Callable
+import requests
 from functools import wraps
 import redis
-import requests
-import requests_html
-import typing
 
 
-def counter(method: typing.Callable):
-    """
-    Counts the calls to the input method
-    """
+def cache_track_response(method: Callable):
+    """Cache and track function response"""
 
     @wraps(method)
-    def count(self, url: str, *args, **kwargs) -> typing.Callable:
-        """
-        Caches the counts of visits to url
-        """
-        key = 'count:' + url
-        self._redis.incr(key)
-        self._redis.expire(key, 10)
-        return counter(url, *args, **kwargs)
-    return count
+    def wrapper(url: str, *args, **kwargs):
+        """Implement the caching and tracking"""
+        count_key = "count:{}".format(url)
+        _redis = redis.Redis()
+        _redis.incr(count_key)
+        result = method(url, *args, **kwargs)
+        _redis.setex(url, timedelta(seconds=10), str(result))
+        return result
+
+    return wrapper
 
 
-class Cache:
-    """
-    This class queries a webpage and monitors the number of visits to it
-    """
-
-    def __init__(self):
-        """
-        Initialize class
-        """
-        self._redis = redis.Redis()
-        self._redis.flushdb()
-
-    @counter
-    def get_page(self, url: str) -> str:
-        """
-        This function ses the requests module to obtain
-        the HTML content of a particular URL and returns it
-        """
-        response = requests.get(url)
-        return response.text
-
-
-if __name__ == '__main__':
-    url = 'http://www.google.com'
-    cache = Cache()
-    print(cache)
-    cache.get_page(url)
+@cache_track_response
+def get_page(url: str) -> str:
+    """Return HTML content of the `url` specified"""
+    response = requests.get(url)
+    return response.text
